1103bff29d8ada07075d231c18adc2e235603805
Yharon0128 <rob.hellemans@gmail.com>
2022-12-09 19:48:31 +0100

    refactored FormulaParser to work with MxParser, unfinished

--- NerdleCLI/RandomFormulaGenerator/src/FormulaParser.java
+++ NerdleCLI/RandomFormulaGenerator/src/FormulaParser.java
@@ -1,3 +1,4 @@
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -5,15 +6,57 @@ import org.mariuszgromada.math.mxparser.*;
 
 public class FormulaParser {
 
-    public static int getResult() {
-        return 0; // TODO: implement mxpaser from Test.java
+    /**
+     *
+     * @param formula the formula in an array of strings WITHOUT A RESULT!
+     * @return result of the formula
+     */
+    public static int getResult(ArrayList<String> formula) {
+        StringBuilder expressionBuilder = new StringBuilder();
+        for (String character : formula) {
+            expressionBuilder.append(character);
+        }
+
+        String expressionString = expressionBuilder.toString();
+
+        Expression e = new Expression(expressionString);
+
+        return (int) e.calculate();
     }
 
-    public static void main(String[] args) {
-        String[] test1 = new String[]{"5", "+", "3", "/", "3", "+", "3", "3", "-", "5", "6", "/", "2"}; // 11
-        String[] test2 = new String[]{"5", "*", "3", "+", "2", "2", "-", "8", "+", "7", "6", "5", "4", "3", "-", "2", "5", "/", "5"}; // 76567
-        String[] test3 = new String[]{"5", "*", "5", "*", "5"};
+    public static boolean validateInput(ArrayList<String> input) {
+        if (!input.contains("=")) {
+            return false; // no result in input
+        }
 
-        System.out.println();
+        if (!input.contains("+") && !input.contains("-") && !input.contains("*") && !input.contains("/")) {
+            return false; // no operator in input
+        }
+
+        StringBuilder expressionString = new StringBuilder();
+
+        for (String character : input) {
+            if (character.equals("")) {
+                break;
+            } else {
+                expressionString.append(character);
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(ArrayList<String> args) {
+        ArrayList<String> test1 = new ArrayList<String>(Arrays.asList("5", "+", "3", "/", "3", "+", "3", "3", "-", "5", "6", "/", "2")); // 11
+        ArrayList<String> test2 = new ArrayList<String>(Arrays.asList("5", "*", "3", "+", "2", "2", "-", "8", "+", "7", "6", "5", "4", "3", "-", "2", "5", "/", "5")); // 76567
+        ArrayList<String> test3 = new ArrayList<String>(Arrays.asList("5", "*", "5", "*", "5")); // 125
+
+        ArrayList<String> test1withResult = new ArrayList<String>(Arrays.asList("5", "+", "3", "/", "3", "+", "3", "3", "-", "5", "6", "/", "2", "=", "11")); // 11
+        ArrayList<String> test2withResult = new ArrayList<String>(Arrays.asList("5", "*", "3", "+", "2", "2", "-", "8", "+", "7", "6", "5", "4", "3", "-", "2", "5", "/", "5", "=", "125")); // 76567
+        ArrayList<String> test3withResult = new ArrayList<String>(Arrays.asList("5", "*", "5", "*", "5", "=", "125"));
+
+        System.out.println(FormulaParser.getResult(test1));
+        System.out.println(FormulaParser.getResult(test2));
+        System.out.println(FormulaParser.getResult(test3));
     }
 }
