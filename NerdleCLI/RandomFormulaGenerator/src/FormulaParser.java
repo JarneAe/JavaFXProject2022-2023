8453ce5b10da4de4c3ad015fb6a4f2b3c6bb0922
Yharon0128 <rob.hellemans@gmail.com>
2022-12-09 20:25:43 +0100

    Removed test files, updated FormulaParser

--- NerdleCLI/RandomFormulaGenerator/src/FormulaParser.java
+++ NerdleCLI/RandomFormulaGenerator/src/FormulaParser.java
@@ -24,6 +24,12 @@ public class FormulaParser {
         return (int) e.calculate();
     }
 
+    /**
+     *
+     * @param input the input from user in an arraylist
+     * @return true if the input passes all checks (has a result, result matches expression, expression contains operators)
+     * and false if it does not
+     */
     public static boolean validateInput(ArrayList<String> input) {
         if (!input.contains("=")) {
             return false; // no result in input
@@ -33,30 +39,47 @@ public class FormulaParser {
             return false; // no operator in input
         }
 
-        StringBuilder expressionString = new StringBuilder();
+        ArrayList<String> expression = new ArrayList<>();
+        StringBuilder resultString = new StringBuilder();
 
-        for (String character : input) {
-            if (character.equals("")) {
+        for (String character : input) { // get expression without the result
+            if (character.equals("=")) {
                 break;
             } else {
-                expressionString.append(character);
+                expression.add(character);
             }
         }
 
-        return true;
+        int startIndexForResult = input.indexOf("=") + 1;
+
+        if (startIndexForResult >= input.size()) { // if "=" is the last character of the input, thus not having a valid result
+            return false;
+        }
+
+        for (int i = startIndexForResult; i < input.size(); i++) { // get result from input
+            resultString.append(input.get(i));
+        }
+
+        return Integer.parseInt(resultString.toString()) == FormulaParser.getResult(expression); // returns true if the result in input matches the result of the expression
     }
 
-    public static void main(ArrayList<String> args) {
+    public static void main(String[] args) {
         ArrayList<String> test1 = new ArrayList<String>(Arrays.asList("5", "+", "3", "/", "3", "+", "3", "3", "-", "5", "6", "/", "2")); // 11
         ArrayList<String> test2 = new ArrayList<String>(Arrays.asList("5", "*", "3", "+", "2", "2", "-", "8", "+", "7", "6", "5", "4", "3", "-", "2", "5", "/", "5")); // 76567
         ArrayList<String> test3 = new ArrayList<String>(Arrays.asList("5", "*", "5", "*", "5")); // 125
 
         ArrayList<String> test1withResult = new ArrayList<String>(Arrays.asList("5", "+", "3", "/", "3", "+", "3", "3", "-", "5", "6", "/", "2", "=", "11")); // 11
-        ArrayList<String> test2withResult = new ArrayList<String>(Arrays.asList("5", "*", "3", "+", "2", "2", "-", "8", "+", "7", "6", "5", "4", "3", "-", "2", "5", "/", "5", "=", "125")); // 76567
+        ArrayList<String> test2withResult = new ArrayList<String>(Arrays.asList("5", "*", "3", "+", "2", "2", "-", "8", "+", "7", "6", "5", "4", "3", "-", "2", "5", "/", "5", "=", "76567")); // 76567
         ArrayList<String> test3withResult = new ArrayList<String>(Arrays.asList("5", "*", "5", "*", "5", "=", "125"));
+        ArrayList<String> test4withResult = new ArrayList<String>(Arrays.asList("5", "*", "5", "*", "5", "="));
 
         System.out.println(FormulaParser.getResult(test1));
         System.out.println(FormulaParser.getResult(test2));
         System.out.println(FormulaParser.getResult(test3));
+
+        System.out.println(FormulaParser.validateInput(test1withResult));
+        System.out.println(FormulaParser.validateInput(test2withResult));
+        System.out.println(FormulaParser.validateInput(test3withResult));
+        System.out.println(FormulaParser.validateInput(test4withResult));
     }
 }
--- NerdleCLI/RandomFormulaGenerator/src/Test.java
+++
@@ -1,8 +0,0 @@
-import org.mariuszgromada.math.mxparser.*;
-
-public class Test {
-    public static void main(String[] args) {
-        Expression e = new Expression("2+1");
-        System.out.println(e.calculate());
-    }
-}
