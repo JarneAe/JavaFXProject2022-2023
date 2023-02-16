c3e48b07d3e7026dd16888943e3a93b41baded36
JarneAe <jarnea408@gmail.com>
2023-02-16 15:50:11 +0100

    writing multiple users to json works :smirk_cat:

--- NerdleGUI/src/be/kdg/nerdle/model/jsonManager/JsonManager.java
+++ NerdleGUI/src/be/kdg/nerdle/model/jsonManager/JsonManager.java
@@ -5,14 +5,15 @@ import com.google.gson.Gson;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class JsonManager {
 
     Gson gson = new Gson();
 
-    public void WriteObjectToJson(User user) throws IOException {
+    public void WriteObjectToJson(List<User> userList) throws IOException {
         try (FileWriter writer = new FileWriter("Users.json",true)) {
-            gson.toJson(user, writer);
+            gson.toJson(userList, writer);
         } catch (IOException e) {
             e.printStackTrace();
 
--- NerdleGUI/src/be/kdg/nerdle/model/jsonManager/Main.java
+++ NerdleGUI/src/be/kdg/nerdle/model/jsonManager/Main.java
@@ -3,14 +3,18 @@ package be.kdg.nerdle.model.jsonManager;
 import be.kdg.nerdle.model.User;
 
 import java.io.IOException;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
         User bart = new User("Bart");
         User bart2 = new User("Bart2");
+
+        List<User> userList = List.of(bart2,bart);
+        System.out.println(userList);
+
         JsonManager manager = new JsonManager();
-        manager.WriteObjectToJson(bart);
-        manager.WriteObjectToJson(bart2);
+        manager.WriteObjectToJson(userList);
     }
 }
