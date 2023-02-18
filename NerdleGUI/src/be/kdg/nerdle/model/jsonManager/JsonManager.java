73cc5006e646906c609521edcb800ebd8cc9850d
Yharon0128 <rob.hellemans@gmail.com>
2023-02-18 14:46:43 +0100

    gson work heheheha

--- NerdleGUI/src/be/kdg/nerdle/model/jsonManager/JsonManager.java
+++ NerdleGUI/src/be/kdg/nerdle/model/jsonManager/JsonManager.java
@@ -2,14 +2,16 @@ package be.kdg.nerdle.model.jsonManager;
 
 import be.kdg.nerdle.model.User;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 public class JsonManager {
 
-    Gson gson = new Gson();
+    Gson gson = new GsonBuilder().registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
 
     public void WriteObjectToJson(List<User> userList) throws IOException {
         try (FileWriter writer = new FileWriter("Users.json")) {
