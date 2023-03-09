1a017f65dc42d151d5bb4116a9f50929f22d79b5
Yharon0128 <rob.hellemans@gmail.com>
2023-03-09 14:38:47 +0100

    fixed something that didn't need fixing because I broke something that didn't need breaking

--- NerdleGUI/src/be/kdg/nerdle/model/jsonManager/JsonManager.java
+++ NerdleGUI/src/be/kdg/nerdle/model/jsonManager/JsonManager.java
@@ -5,10 +5,7 @@ import be.kdg.nerdle.model.UserList;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -18,7 +15,7 @@ import java.util.Objects;
 public class JsonManager {
     /**
      * only this gson object should be used to interface with Users.json, or gson objects created exactly like this,
-     *      as without the TypeAdapter for LocalDate, writing to Users.json simply will not work.
+     * as without the TypeAdapter for LocalDate, writing to Users.json simply will not work.
      */
     private static final Gson gson = new GsonBuilder()
             .setPrettyPrinting()
@@ -30,6 +27,7 @@ public class JsonManager {
     /**
      * Writes a single user to the Users.json file in the root of the project. This method checks if a user with the given
      * username already exists, and will not write the user to the json if this is the case
+     *
      * @param user the user to write to the json file
      */
     public static void writeUserToJson(User user) {
@@ -46,6 +44,7 @@ public class JsonManager {
 
     /**
      * private method to write a UserList object to the json file
+     *
      * @param userList UserList object, typically read from the json beforehand. WARNING: if this is a new UserList object,
      *                 it will truncate the Users.json file!
      */
@@ -62,6 +61,7 @@ public class JsonManager {
 
     /**
      * private method for getting all users from the json in the form of a UserList object
+     *
      * @return UserList object with the contents of the Users.json file
      */
 
@@ -82,6 +82,7 @@ public class JsonManager {
 
     /**
      * method for getting a particular user out of the json file
+     *
      * @param name the name of the user you are trying to get. Case-sensitive!
      * @return the User object. if the name does not match any of the entries in the json file, this will return null!
      */
@@ -96,16 +97,18 @@ public class JsonManager {
 
     /**
      * method to add a score entry to a user in the json file
-     * @param name name of the user you are trying to update. Case-sensitive!
-     * @param date LocalDate of the date you are trying to assign a score for. Normally always LocalDate.now(), but
-     *             this was kept in for testing purposes
+     *
+     * @param name  name of the user you are trying to update. Case-sensitive!
+     * @param date  LocalDate of the date you are trying to assign a score for. Normally always LocalDate.now(), but
+     *              this was kept in for testing purposes
      * @param tries integer of the number of tries you are trying to assign
      */
     public static void addToOutcomesByName(String name, LocalDate date, int tries) {
         UserList userList = getAllUsersFromJson();
 
         for (int i = 0; i < Objects.requireNonNull(userList).getUsers().size(); i++) {
-            if (userList.getUsers().get(i).getName().equals(name)) userList.getUsers().get(i).addToOutcomes(date, tries);
+            if (userList.getUsers().get(i).getName().equals(name))
+                userList.getUsers().get(i).addToOutcomes(date, tries);
         }
 
         writeUserListToJson(userList);
@@ -113,6 +116,7 @@ public class JsonManager {
 
     /**
      * method to check if a name is available or not
+     *
      * @param name name you are trying to check is available
      * @return True if the name is taken, false if it is not
      */
@@ -131,15 +135,26 @@ public class JsonManager {
     /**
      * method intended for use on the "enter username" screen. this method handles users who already exist, and
      * record new users into the Users.json file
+     *
      * @param name name of the user. Input from text field, probably
      * @return User object, either read from Users.json or newly created
      */
     public static User handleEnteredUsername(String name) {
+        initialize();
         if (nameTaken(name)) return getUserByName(name);
         else {
             User user = new User(name);
             writeUserToJson(user);
             return user;
         }
+    }
+
+    private static void initialize() {
+        try {
+            File tmpFile = pathToUsersFile.toFile();
+            if (!tmpFile.exists()) {
+                boolean ignored = tmpFile.createNewFile();
+            }
+        } catch (IOException ignored) {}
     }
 }
